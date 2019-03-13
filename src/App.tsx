75d28271bf76ce063ebeec7f2d8f910268e69a96
Tantatorn Suksangwarn <timefear007@gmail.com>
2019-03-13 23:44:07 +0700

    Add Menu Bar

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
 import './App.css';
+import MenuBar from './Assets/MenuBar';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        
+        <MenuBar />        
       </div>
     );
   }
---
+++ src/Assets/MenuBar.tsx
@@ -0,0 +1,46 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import AppBar from '@material-ui/core/AppBar';
+import  { createStyles, WithStyles, withStyles } from '@material-ui/core/styles';
+import MenuIcon from '@material-ui/icons/Menu';
+import { Toolbar, IconButton, Typography } from '@material-ui/core';
+
+const styles = createStyles({
+    root: {
+        flexGrow: 1,
+    },
+    grow: {
+        flexGrow: 1,
+    },
+    MenuButton: {
+        marginLeft: -12,
+        marginRight: 20,
+    }
+});
+
+export interface Props extends WithStyles<typeof styles> {}
+
+function MenuBar(props: Props) {
+    const {classes} = props;
+    return (
+    <div className={classes.root}>
+        <AppBar>
+            <Toolbar>
+                <IconButton className={classes.MenuButton} color="inherit" aria-label="Menu">
+                    <MenuIcon />
+                </IconButton>
+                <Typography variant="h6" color="inherit" className={classes.grow}>
+                    Home
+                </Typography>
+            </Toolbar>
+        </AppBar>                 
+    </div>
+    )
+}
+
+MenuBar.propTypes = {
+    classes: PropTypes.object.isRequired,
+} as any;
+
+export default withStyles(styles)(MenuBar);
